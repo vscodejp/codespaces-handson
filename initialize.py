dd3201e304dea60e268b17432a0d472b9eb98b34
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2022-12-29 17:15:21 +0900

    MySQLのパスワードを運用しない方針に変更

--- initialize.py
+++ initialize.py
@@ -4,23 +4,44 @@ import os
 import sys
 from os import path
 import subprocess
+import time
 
 
-def run_cmd(command: list[str]):
-    print("\033[1m" + " ".join(command) + "\033[0m")
-    subprocess.run(command, check=True)
+def run(cmd: list[str]):
+    """ コマンド実行時スニペット """
+    print("\033[1m" + " ".join(cmd) + "\033[0m")
+    subprocess.run(cmd, check=True)
+
+
+def wait_mysql_started():
+    """ mysql の起動まで待機 """
+    print("wait mysql start", end="")
+    while True:
+        print(".", end="")
+        result = subprocess.run(["mysql", "-h", "mysql", "-u", "root", "-e", "SHOW DATABASES;"], check=False, capture_output=True)
+        if result.returncode > 0:
+            time.sleep(1)
+            continue
+        if str(result.stdout).count("main") > 0:
+            break
+        time.sleep(1)
+    print()
 
 venv_path = path.join(os.getcwd(), ".venv")
 if not path.exists(venv_path):
     # venv がない場合は作る
-    run_cmd(["env", "python", "-m", "venv", venv_path])
+    run(["env", "python", "-m", "venv", venv_path])
 
     # 依存スクリプトの実行
-    run_cmd(["env", "poetry", "install"])
+    run(["env", "poetry", "install"])
 
     # vnv 内での実行に切り替え
-    run_cmd(["poetry", "run", path.join(".", "initialize.py")])
+    run(["poetry", "run", path.join(".", "initialize.py")])
 
     sys.exit(0)
 
-run_cmd(["mysql", "-h", "mysql", "main", "-e", "source ./database/ddl.sql"])
+wait_mysql_started()
+
+run(["mysql", "-h", "mysql", "-u", "root", "main", "-e", "source ./database/ddl.sql"])
+
+run(["mysql", "-h", "mysql", "-u", "root", "main", "-e", "source ./database/data.sql"])
